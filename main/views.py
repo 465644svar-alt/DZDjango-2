1040adeba1cd448e58ddfb091eac351c83da9f0d
465644svar-alt <465644svar@gmail.com>
2026-02-09 20:26:49 +0300

    its work

---
+++ main/views.py
@@ -0,0 +1,17 @@
+
+# main/views.py
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def services(request):
+    return render(request, 'main/services.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
+
+# Create your views here.
